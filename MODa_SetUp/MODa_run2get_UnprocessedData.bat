28121b8e7d83d506957a16518714ce30c38c0278
Viswanadham Sridhara <vsridhara@utexas.edu>
2015-07-02 10:09:03 -0500

    Complete set of MODa glucose 4th replicate ..
    … and updating other MODa_setup files

---
+++ MODa_SetUp/MODa_run2get_UnprocessedData.bat
@@ -0,0 +1,1 @@
+java -jar MODa_v1.23.jar -i MURI_100b.txt -o PSMs/MURI_100b_MODa_PSMs.txt
---
+++ MODa_SetUp/MODa_runOnUnprocessed2getProcessedOutput.bat
@@ -0,0 +1,1 @@
+java -jar anal_moda.jar -i PSMs/MURI_100b_MODa_PSMs.txt -p MURI_100b.txt -fdr 0.01 -d REV### 
